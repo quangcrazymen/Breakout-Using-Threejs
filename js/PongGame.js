7aba919ccfe7277a93dc48887d5749ffcd39db1e
quangcrazymen <Quangcrazymen@gmail.com>
2022-04-27 21:41:16 +0700

    resize ball and add object

--- js/PongGame.js
+++ js/PongGame.js
@@ -1,4 +1,4 @@
-init=()=>{
+(()=>{
     const scene = new THREE.Scene()
     console.log(THREE.REVISION)
     const camera = new THREE.PerspectiveCamera(
@@ -16,21 +16,23 @@ init=()=>{
     camera.up.set(0, 0, -1);
     camera.lookAt(0, 0, 0);
 
-    //Add ball 3
-    const ball3 = getSphere(1)
-    ball3.name = 'ball-3'
-    scene.add(ball3)
+    //Add ball
+    const ball = getSphere(0.5)
+    scene.add(ball)
+    ball.position.y=0.5
     //Add plane
     const plane = getPlane(25)
     scene.add(plane)
     plane.name= 'plane'
     plane.rotation.x = Math.PI/2
+
     //Add lighting
     const pointLight = getPointLight(2)
     scene.add(pointLight)
     pointLight.position.y=10
-    const lightBulb=getSphere(0.5)
+    const lightBulb=getSphere(0.05)
     pointLight.add(lightBulb)
+
     //Add a Paddle
     const paddle = getBox(7,1,0.5)
     scene.add(paddle)
@@ -40,15 +42,52 @@ init=()=>{
     let testBox = getBox(1,1,1)
     scene.add(testBox)
     testBox.name = 'testBox'
-    
-    //Add grid of target
-    const targets = getGridOfBoxes(16,3)
-    scene.add( targets)
-    setBoundingBox(targets)
-    console.log(BBArray)
 
-    initInput()
-    console.log(plane.geometry.parameters)
+    //Add Target Geometry
+    const coneGeometry = getCone(1,2,16)
+    coneGeometry.position.x=4
+    coneGeometry.position.y=1
+    scene.add(coneGeometry)
+
+    const dodecahedronGeometry= getDodecahedron(1)
+    dodecahedronGeometry.position.x=-4
+    dodecahedronGeometry.position.y=1
+    scene.add(dodecahedronGeometry)
+
+    const heartGeometry = getHeart()
+    scene.add(heartGeometry)
+    heartGeometry.scale.x=0.2
+    heartGeometry.scale.y=0.2
+    heartGeometry.scale.z=0.2
+    heartGeometry.rotation.x = Math.PI/2
+    heartGeometry.position.y = 1
+    heartGeometry.position.z = -6
+
+    const torusGeometry = getTorus()
+    scene.add(torusGeometry)
+    torusGeometry.scale.x=0.2
+    torusGeometry.scale.y=0.2
+    torusGeometry.scale.z=0.2
+    torusGeometry.rotation.x = Math.PI/2
+    torusGeometry.position.z = -6
+    torusGeometry.position.y = 1
+    torusGeometry.position.x = -7
+    const box = new THREE.BoxHelper( torusGeometry, 0xffff00 )
+    scene.add(box)
+    console.log(torusGeometry)
+
+    const torusKnotGeometry = getTorusKnot()
+    scene.add(torusKnotGeometry)
+    torusKnotGeometry.scale.x=0.2
+    torusKnotGeometry.scale.y=0.2
+    torusKnotGeometry.scale.z=0.2
+    torusKnotGeometry.rotation.x = Math.PI/2
+    torusKnotGeometry.position.z = -6
+    torusKnotGeometry.position.y = 1
+    torusKnotGeometry.position.x = 7
+    let torusObject = new THREE.Box3(new THREE.Vector3(),new THREE.Vector3())
+    torusObject.setFromObject(torusGeometry)
+    //testBoxBB.copy(targets.children[i].geometry.boundingBox).applyMatrix4(targets.children[i].matrixWorld)
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth,window.innerHeight);
@@ -58,11 +97,73 @@ init=()=>{
     //Initiate Controls
     const control=new THREE.OrbitControls(camera,renderer.domElement)
    
+    //Control ball 3
+    let upRight = 0
+    let downLeft = 1
+    let downRight = 0
+    let upLeft =0
+
+    let ballBB = new THREE.Sphere(new THREE.Vector3(0,0,0),1)
+
+    ballTrajectory = ()=>{
+        if(upRight ===1){
+            upRight = 0
+            downRight = 1
+        }
+        else if(downRight ===1){
+            downRight = 0
+            upRight =1
+        }
+        else if(upLeft ===1){
+            upLeft=0
+            downLeft=1
+        }
+        else if(downLeft ===1){
+            downLeft=0
+            upLeft = 1
+        }
+    }
+
+    function checkCollisions(ball,box){
+        if(ballBB.intersectsBox(box)){
+            ball.material.transparent = true
+            ball.material.opacity = 0.5
+            ball.material.color = new THREE.Color(Math.random()*0xffffff)
+            //ballTrajectory()  
+            return 1
+        }
+        else{
+            ball.material.opacity=1.0   
+        }
+        
+    }
+
+    //list of individual targets
+    //const targetsBB_0 = new THREE.Box3(new THREE.Vector3(),new THREE.Vector3())
+    //targetsBB_0.setFromObject(targets)
+    
+    //const box = new THREE.BoxHelper( targets.children[0], 0xffff00 )
+    // for(let i=1;i<targets.children.length;i++){
+    //     console.log(targets.children[i])
+    //     testBoxBB = new THREE.Box3(new THREE.Vector3(),new THREE.Vector3())
+    //     testBoxBB.setFromObject(targets.children[i])
+    //     testBoxBB.copy(targets.children[i].geometry.boundingBox).applyMatrix4(targets.children[i].matrixWorld)
+    //     // console.log(targets.children[i])
+    //     const box = new THREE.BoxHelper( targets.children[i], 0xffff00 )
+    //     scene.add(box)
+    // }
+    const gui = new dat.GUI();
+
+    gui.add(controls, 'ballSpeed', 0, 1);
+    //gui.add(controls, 'bouncingSpeed', 0, 0.5);
+    //gui.add(controls, 'toggleFunction',0,1)
+    
     update= (renderer,scene,camera,control)=>{
         renderer.render(
             scene,
             camera
         )
+
         // Trajectory of the ball when it hit a paddle
         //https://gamedev.stackexchange.com/questions/4253/in-pong-how-do-you-calculate-the-balls-direction-when-it-bounces-off-the-paddl
     
@@ -70,241 +171,176 @@ init=()=>{
         // move ball in 45 degree: https://stackoverflow.com/questions/65534926/three-js-move-object-using-vector-with-applied-angle
         const planeWidth = plane.geometry.parameters.width
         const planeHeight = plane.geometry.parameters.height
-        const paddle = scene.getObjectByName('paddle')
-    
-        //Control ball 3
-        const ball3 = scene.getObjectByName('ball-3')
+        
         if(upRight === 1){
-            if(Math.abs(ball3.position.z)>= planeHeight/2){
+            if(Math.abs(ball.position.z)>= planeHeight/2){
                 upRight=0
                 downRight = 1
                 //To prevent the ball from stucking in the boundary
-                DegreeDownLeft(ball3)
+                DegreeDownLeft(ball)
             }
-            else if(Math.abs(ball3.position.x)>= planeWidth/2){
+            else if(Math.abs(ball.position.x)>= planeWidth/2){
                 upLeft=1
                 upRight=0
-                DegreeDownLeft(ball3)
+                DegreeDownLeft(ball)
             }
             else{
-                DegreeUpRight(ball3)  
+                DegreeUpRight(ball)  
             }
         }
         else if(downRight === 1){
-            if(Math.abs(ball3.position.x)>= planeWidth/2){
+            if(Math.abs(ball.position.x)>= planeWidth/2){
                 downRight=0
                 downLeft = 1
-                DegreeUpLeft(ball3)
+                DegreeUpLeft(ball)
             }
-            else if(Math.abs(ball3.position.z)>= planeHeight/2){
+            else if(Math.abs(ball.position.z)>= planeHeight/2){
                 upRight = 1
                 downRight = 0
-                DegreeUpLeft(ball3)
+                DegreeUpLeft(ball)
             }
             else{
-                DegreeDownRight(ball3)  
+                DegreeDownRight(ball)  
             }
         }
         else if(downLeft === 1){
-            if(Math.abs(ball3.position.x)>= planeWidth/2 ){
+            if(Math.abs(ball.position.x)>= planeWidth/2 ){
                 downRight=1
                 downLeft=0
-                DegreeUpRight(ball3)
+                DegreeUpRight(ball)
             }
-            else if(Math.abs(ball3.position.z)>= planeHeight/2){
+            else if(Math.abs(ball.position.z)>= planeHeight/2){
                 upLeft= 1
                 downLeft=0 
-                DegreeUpRight(ball3)
+                DegreeUpRight(ball)
             }
             else{
-                DegreeDownLeft(ball3)  
+                DegreeDownLeft(ball)  
             }
         }
         else if(upLeft === 1){
-            if(Math.abs(ball3.position.x)>= planeWidth/2 ){
+            if(Math.abs(ball.position.x)>= planeWidth/2 ){
                 upLeft=0
                 upRight = 1
-                DegreeDownRight(ball3)
+                DegreeDownRight(ball)
             }
-            else if(Math.abs(ball3.position.z)>= planeHeight/2){
+            else if(Math.abs(ball.position.z)>= planeHeight/2){
                 downLeft=1
                 upLeft=0
-                DegreeDownRight(ball3)
+                DegreeDownRight(ball)
             }
             else{
-                DegreeUpLeft(ball3)  
+                DegreeUpLeft(ball)  
             }
         }
     
+
         //If stuck try to implement this: https://www.youtube.com/watch?v=9H3HPq-BTMo
-        if(ball3.position.z>0){
-            let ballPositionZ = 12.5 - Math.abs(ball3.position.z)
-            let ballPositionX = ball3.position.x +25
+        if(ball.position.z>0){
+            let ballPositionZ = 12.5 - Math.abs(ball.position.z)
+            let ballPositionX = ball.position.x +25
             let paddlePositionX = paddle.position.x+25
             let deltaX = Math.abs(paddlePositionX-ballPositionX)
             let distanceOfBallAndPaddle = Math.sqrt(ballPositionZ*ballPositionZ+deltaX*deltaX)
                 
             if(distanceOfBallAndPaddle<2){
-                ball3.material.transparent = true
-                ball3.material.opacity = 0.5
-                ball3.material.color = new THREE.Color(Math.random()*0xffffff)
+                ball.material.transparent = true
+                ball.material.opacity = 0.5
+                ball.material.color = new THREE.Color(Math.random()*0xffffff)
+                if(downLeft===1){
+                    downLeft=0
+                    upLeft=1
+                }
+                else if(downRight===1){
+                    downRight=0
+                    upRight=1
+                }
             }
             else{
-                ball3.material.opacity=1.0
+                ball.material.opacity=1.0
             }
             //console.log(distanceOfBallAndPaddle)
         }
         //create bounding box for the ball
-        let testBox = scene.getObjectByName('testBox')
-        testBoxBB.setFromObject(testBox)
-        ballBB.copy(ball3.geometry.boundingSphere).applyMatrix4(ball3.matrixWorld)
-    
-        // compute bounding box:
-        const targets = scene.getObjectByName('targets')
-    
-        //list of individual targets
-        const targetsBB_0 = new THREE.Box3(new THREE.Vector3(),new THREE.Vector3())
-        targetsBB_0.setFromObject(targets.children[0])
-        if(checkCollisions(ball3, targetsBB_0)){
-            targets.children[0].visible = false
+        //let testBox = scene.getObjectByName('testBox')
+        //testBoxBB.setFromObject(testBox)
+        ballBB.copy(ball.geometry.boundingSphere).applyMatrix4(ball.matrixWorld)
+        if(torusGeometry.visible === true){
+            if(checkCollisions(ball, torusObject))
+                torusGeometry.visible = false
         }
     
-        const box = new THREE.BoxHelper( targets.children[3], 0xffff00 )
-        scene.add(box)
-        checkCollisions(ball3, testBoxBB)
-    
-    
         // THREE JS PICKING: https://r105.threejsfundamentals.org/threejs/lessons/threejs-picking.html
-        //console.log(targets)
-    
+        
         control.update()
         requestAnimationFrame(()=>update(renderer,scene,camera,control))
     }
     update(renderer,scene,camera,control)
-    return scene
-    
-}
+
+    //Add control for the paddle
+    ;(()=>{
+        this.keys_ = {
+            right:false,
+            left:false
+        }
+        this.oldKeys={...this.keys_,}
+
+
+        document.addEventListener('keydown',(e)=>this.OnKeyDown(e))
+        document.addEventListener('keyup',(e)=>this.OnKeyUp(e))
+
+    })()
+
+    OnKeyDown = (Event)=>{
+        const paddle = scene.getObjectByName('paddle')
+        switch (Event.keyCode){
+            case 68:
+                paddle.position.x+=0.5
+                this.keys_.right=true
+                break
+            case 65:
+                paddle.position.x-=0.5
+                this.keys_.left=true
+                break
+        }
+    }
+    OnKeyUp=(Event)=>{
+        switch (Event.keyCode){
+            case 68:
+                this.keys_.right=false
+                break
+            case 65:
+                this.keys_.left=false
+                break
+        }
+    }   
+})()
 //group of target box
 
-let upRight = 0
-let downLeft = 1
-let downRight = 0
-let upLeft =0
 
-DegreeDownRight = (object)=>{
-    totalrotation=45
-    v = new THREE.Vector3(0, 0, 0.1)
-    rads = THREE.Math.degToRad(totalrotation)
-    v.applyAxisAngle( new THREE.Vector3(0, 1, 0), rads);
-    object.position.add(v)
+
+// BBArray = []
+// setBoundingBox = (object)=>{
+//     for(let i = 0 ;i<64;i++){
+//         let BB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3())
+//         BB.setFromObject(object.children[i])
+//         BBArray.push(BB)
+//     }
+//     return BBArray
+// }
+
+
+//Test javascript
+//Closure ?
+;function counter(){
+    let counter = 0 
+    function increment(){
+        return counter++
+    } 
+    return increment
 }
-DegreeUpLeft = (object)=>{
-    totalrotation=45
-    v = new THREE.Vector3(0, 0, -0.1)
-    rads = THREE.Math.degToRad(totalrotation)
-    v.applyAxisAngle( new THREE.Vector3(0, 1, 0), rads);
-    object.position.add(v)
-}
-DegreeDownLeft=(object)=>{
-    totalrotation=-45
-    v = new THREE.Vector3(0, 0, 0.1)
-    rads = THREE.Math.degToRad(totalrotation)
-    v.applyAxisAngle( new THREE.Vector3(0, 1, 0), rads);
-    object.position.add(v)
-}
-
-DegreeUpRight=(object)=>{
-    totalrotation=-45
-    v = new THREE.Vector3(0, 0, -0.1)
-    rads = THREE.Math.degToRad(totalrotation)
-    v.applyAxisAngle( new THREE.Vector3(0, 1, 0), rads);
-    object.position.add(v)
-}
-
-ballTrajectory = ()=>{
-    if(upRight ===1){
-        upRight = 0
-        downRight = 1
-    }
-    else if(downRight ===1){
-        downRight = 0
-        upRight =1
-    }
-    else if(upLeft ===1){
-        upLeft=0
-        downLeft=1
-    }
-    else if(downLeft ===1){
-        downLeft=0
-        upLeft = 1
-    }
-}
-
-function checkCollisions(ball,box){
-    if(ballBB.intersectsBox(box)){
-        ball.material.transparent = true
-        ball.material.opacity = 0.5
-        ball.material.color = new THREE.Color(Math.random()*0xffffff)
-        ballTrajectory()    
-        return 1
-    }
-    else{
-        ball.material.opacity=1.0   
-    }
-    
-}
-
-let ballBB = new THREE.Sphere(new THREE.Vector3(0,0,0),1)
-let testBoxBB = new THREE.Box3(new THREE.Vector3(),new THREE.Vector3())
-
-BBArray = []
-setBoundingBox = (object)=>{
-    for(let i = 0 ;i<64;i++){
-        let BB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3())
-        BB.setFromObject(object.children[i])
-        BBArray.push(BB)
-    }
-    return BBArray
-}
-
-
-//Add control for the paddle
-initInput = ()=>{
-    this.keys_ = {
-        right:false,
-        left:false
-    }
-    this.oldKeys={...this.keys_,}
-
-
-    document.addEventListener('keydown',(e)=>this.OnKeyDown(e))
-    document.addEventListener('keyup',(e)=>this.OnKeyUp(e))
-
-}
-
-OnKeyDown = (Event)=>{
-    const paddle = scene.getObjectByName('paddle')
-    switch (Event.keyCode){
-        case 68:
-            paddle.position.x+=0.5
-            this.keys_.right=true
-            break
-        case 65:
-            paddle.position.x-=0.5
-            this.keys_.left=true
-            break
-    }
-}
-OnKeyUp=(Event)=>{
-    switch (Event.keyCode){
-        case 68:
-            this.keys_.right=false
-            break
-        case 65:
-            this.keys_.left=false
-            break
-    }
-}
-const scene=init()
-
+const a=counter()
+console.log(a())
+console.log(a())
+console.log(a)
 
--- js/utils.js
+++ js/utils.js
@@ -18,7 +18,7 @@ function getBox(w,h,d){
 
 function getSphere(r){
     const geometry = new THREE.SphereGeometry(r,24,24);
-    const material= new THREE.MeshBasicMaterial({
+    const material= new THREE.MeshPhongMaterial({
         color: 'rgb(255,0,255)'
     })
     const mesh =new THREE.Mesh(
@@ -30,7 +30,7 @@ function getSphere(r){
 
 getPlane = (size)=>{
     const geometry = new THREE.PlaneGeometry(2*size,size);
-    const material= new THREE.MeshBasicMaterial({
+    const material= new THREE.MeshStandardMaterial({
         color: 'rgb(0,0 ,120)',
         side: THREE.DoubleSide
     })
@@ -42,9 +42,103 @@ getPlane = (size)=>{
     return mesh
 }
 
+getCone =  (radius,height,radialSegments)=>{
+    const geometry = new THREE.ConeGeometry(radius, height, radialSegments);
+    const material= new THREE.MeshPhongMaterial({
+        color: 'rgb(255,0,255)'
+    })
+    const mesh =new THREE.Mesh(
+        geometry,
+        material
+    )
+    return mesh
+}   
+
+getDodecahedron = (radius)=>{
+    const geometry = new THREE.DodecahedronGeometry(radius);
+    const material= new THREE.MeshPhongMaterial({
+        color: 'rgb(2,12,255)'
+    })
+    const mesh =new THREE.Mesh(
+        geometry,
+        material
+    )
+    return mesh
+}
+
+getHeart = ()=>{
+    const shape = new THREE.Shape();
+    const x = -2.5;
+    const y = -5;
+    shape.moveTo(x + 2.5, y + 2.5);
+    shape.bezierCurveTo(x + 2.5, y + 2.5, x + 2, y, x, y);
+    shape.bezierCurveTo(x - 3, y, x - 3, y + 3.5, x - 3, y + 3.5);
+    shape.bezierCurveTo(x - 3, y + 5.5, x - 1.5, y + 7.7, x + 2.5, y + 9.5);
+    shape.bezierCurveTo(x + 6, y + 7.7, x + 8, y + 4.5, x + 8, y + 3.5);
+    shape.bezierCurveTo(x + 8, y + 3.5, x + 8, y, x + 5, y);
+    shape.bezierCurveTo(x + 3.5, y, x + 2.5, y + 2.5, x + 2.5, y + 2.5);
+
+    const extrudeSettings = {
+    steps: 2,  // ui: steps
+    depth: 2,  // ui: depth
+    bevelEnabled: true,  // ui: bevelEnabled
+    bevelThickness: 1,  // ui: bevelThickness
+    bevelSize: 1,  // ui: bevelSize
+    bevelSegments: 2,  // ui: bevelSegments
+};
+
+    const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+    const material= new THREE.MeshPhongMaterial({
+        color: 'rgb(2,12,255)'
+    })
+    const mesh =new THREE.Mesh(
+        geometry,
+        material
+    )
+    return mesh
+}
+
+getTorus = ()=>{
+    const radius = 5;  // ui: radius
+    const tubeRadius = 2;  // ui: tubeRadius
+    const radialSegments = 8;  // ui: radialSegments
+    const tubularSegments = 24;  // ui: tubularSegments
+    const geometry = new THREE.TorusGeometry(
+    radius, tubeRadius,
+    radialSegments, tubularSegments);
+
+    const material= new THREE.MeshPhongMaterial({
+        color: 'rgb(2,122,25)'
+    })
+    const mesh =new THREE.Mesh(
+        geometry,
+        material
+    )
+
+    return mesh
+}
+
+getTorusKnot = ()=>{
+    const radius = 3.5;  // ui: radius
+    const tubeRadius = 1.5;  // ui: tubeRadius
+    const radialSegments = 8;  // ui: radialSegments
+    const tubularSegments = 64;  // ui: tubularSegments
+    const p = 2;  // ui: p
+    const q = 3;  // ui: q
+    const geometry = new THREE.TorusKnotGeometry(
+    radius, tubeRadius, tubularSegments, radialSegments, p, q);
+    const material= new THREE.MeshPhongMaterial({
+        color: 'rgb(26,12,2)'
+    })
+    const mesh =new THREE.Mesh(
+        geometry,
+        material
+    )
+    return mesh
+}
+
 getGridOfBoxes = (amount,separationMultiplier)=>{
     const group= new THREE.Group()
-    group.name = "targets"
     for(let i = 0; i<amount;i++){
         let obj  =getBox(2,1,1)
         obj.position.x = i * separationMultiplier
@@ -60,4 +154,59 @@ getGridOfBoxes = (amount,separationMultiplier)=>{
     group.position.z = -(separationMultiplier*(amount-1.75))/2+11
 
     return group
+}
+
+generateTarget = (amount,separationMultiplier)=>{
+    const group = new THREE.Group()
+    group.translateX(-(separationMultiplier*(amount-1))/2)
+    for(let i= 0;i<amount;i++){
+        let obj  =getBox(2,1,1)
+        obj.position.x = i * separationMultiplier
+        group.add(obj)
+        for(let j =1;j<amount/2;j++){
+            let obj  = getBox(2,1,1)
+            obj.position.x = i * (separationMultiplier)
+            obj.position.z = j * (separationMultiplier)
+            group.add(obj)
+        }
+    }
+    
+    return group
+}
+
+var controls = new function () {
+    this.ballSpeed = 0.1;
+    //this.bouncingSpeed = 0.03;
+    //this.toggleFunction = 0;
+};
+
+//Movement
+DegreeDownRight = (object)=>{
+    totalrotation=45
+    v = new THREE.Vector3(0, 0, controls.ballSpeed)
+    rads = THREE.Math.degToRad(totalrotation)
+    v.applyAxisAngle( new THREE.Vector3(0, 1, 0), rads);
+    object.position.add(v)
+}
+DegreeUpLeft = (object)=>{
+    totalrotation=45
+    v = new THREE.Vector3(0, 0, -controls.ballSpeed)
+    rads = THREE.Math.degToRad(totalrotation)
+    v.applyAxisAngle( new THREE.Vector3(0, 1, 0), rads);
+    object.position.add(v)
+}
+DegreeDownLeft=(object)=>{
+    totalrotation=-45
+    v = new THREE.Vector3(0, 0, controls.ballSpeed)
+    rads = THREE.Math.degToRad(totalrotation)
+    v.applyAxisAngle( new THREE.Vector3(0, 1, 0), rads);
+    object.position.add(v)
+}
+
+DegreeUpRight=(object)=>{
+    totalrotation=-45
+    v = new THREE.Vector3(0, 0, -controls.ballSpeed)
+    rads = THREE.Math.degToRad(totalrotation)
+    v.applyAxisAngle( new THREE.Vector3(0, 1, 0), rads);
+    object.position.add(v)
 }
